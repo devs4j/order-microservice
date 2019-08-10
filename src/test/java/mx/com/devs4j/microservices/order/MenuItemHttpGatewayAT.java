44df419f8ccb39177e3be677281e2226c6d2cf18
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-10 12:00:52 -0500

    Defined pact

--- src/test/java/mx/com/devs4j/microservices/order/MenuItemHttpGatewayAT.java
+++ src/test/java/mx/com/devs4j/microservices/order/MenuItemHttpGatewayAT.java
@@ -1,14 +1,21 @@
 package mx.com.devs4j.microservices.order;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+import org.junit.Assert;
 import org.junit.Rule;
+import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.client.RestTemplate;
 
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
+import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.model.RequestResponsePact;
+import mx.com.devs4j.microservices.order.item.MenuItem;
+import mx.com.devs4j.microservices.order.item.MenuItemGateway;
 
 public class MenuItemHttpGatewayAT {
 	
@@ -17,14 +24,33 @@ public class MenuItemHttpGatewayAT {
 	
 	@Pact(consumer = "order")
 	public RequestResponsePact getMenuItems(PactDslWithProvider provider) throws IOException {
-		return provider.given("get all menu items")
-				.uponReceiving("when all menu items are requested")
-				.path("/items")
+		HashMap<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
+		
+		return provider.given("get one menu item")
+				.uponReceiving("when one menu item is requested")
+				.path("/items/1")
 				.method("GET")
 				.willRespondWith()
 				.status(HttpStatus.OK.value())
+				.headers(headers)
 				.body(TestUtils.readFileAsString("menuItems_getAll.json"))
 				.toPact();
+	}
+	
+	@Test
+	@PactVerification(value = "menuItem", fragment="getMenuItems")
+	public void shouldGetAllMenuItems() {
+		MenuItemGateway menuItemGateway = new MenuItemGateway(getRestTemplate(), 
+				provider.getUrl() + "/items");
+		
+		MenuItem menuItem = menuItemGateway.getMenuItem(1);
+		
+		Assert.assertEquals(Integer.valueOf(1), menuItem.getId());
+	}
+	
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
 	}
 
 }
