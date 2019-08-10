c92290209afe8448d15263b6cc33cab7b49086a6
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-10 17:35:12 -0500

    Enabled client discovery

--- src/main/java/mx/com/devs4j/microservices/order/OrderApplication.java
+++ src/main/java/mx/com/devs4j/microservices/order/OrderApplication.java
@@ -2,7 +2,9 @@ package mx.com.devs4j.microservices.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderApplication {
 
