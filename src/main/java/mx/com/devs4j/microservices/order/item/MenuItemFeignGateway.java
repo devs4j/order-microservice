d149f0b09d4dab5c1592597ec010eef70a186a0e
Francisco Navarrete <francisco.navarrete@walmart.com>
2019-08-16 22:04:59 -0500

    Removed security from order microservice

--- src/main/java/mx/com/devs4j/microservices/order/item/MenuItemFeignGateway.java
+++ src/main/java/mx/com/devs4j/microservices/order/item/MenuItemFeignGateway.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("menu")
+@FeignClient("MENU")
 public interface MenuItemFeignGateway {
 
 	@RequestMapping(
